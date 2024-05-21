464c07bef5d8bc102326b585e5e1dc1123ee1949
Dean Longmire <deanlongmire100@gmail.com>
2024-05-21 11:56:52 -0400

    Init

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'apartment-lister/app';
+import config from 'apartment-lister/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
