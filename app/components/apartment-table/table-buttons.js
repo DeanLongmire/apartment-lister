5e9ee96d8541683f2a3bbe7888ef6f3ef95ef49f
Dean Longmire <deanlongmire100@gmail.com>
2024-05-22 14:34:50 -0400

    Yes/No buttons update dbf

--- app/components/apartment-table/table-buttons.js
+++ app/components/apartment-table/table-buttons.js
@@ -1,13 +1,24 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ApartmentTableTableButtonsComponent extends Component {
+  @service database;
   @tracked isPressed = this.args.value;
 
   @action
   pressed() {
-    console.log(this.isPressed);
     this.isPressed = !this.isPressed;
+    
+    let tableData = this.args.model;
+    let rowEntryName = this.args.entry;
+
+    const body = {
+      valueToUpdate: this.args.column,
+      value: this.isPressed
+    }
+
+    this.database.update(body, rowEntryName);
   }
 }
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
-    async model() {
-        const response = await fetch("/db.json");
-        const data = await response.json()
+  @service database;
 
-        return data.apartments;
-    }
+  async model() {
+    const response = await this.database.get();
+    const data = await response.json();
+
+    console.log(data);
+
+    return data;
+  }
 }
